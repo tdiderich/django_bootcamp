52d2ca943ffef307b5fe5f28b8fdb135cf88e882
Tyler Diderich <tylerdiderich@Tylers-MBP.hsd1.ca.comcast.net>
2017-05-01 19:31:28 -0700

    add blog template

---
+++ tdiderich_blog/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/tylerdiderich/Desktop/django_bootcamp/virtualenvs/tdiderich_blog/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
